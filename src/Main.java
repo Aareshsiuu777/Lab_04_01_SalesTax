f58646246e7f5ded704989dfc31fe4607678a956
Aareshsiuu777 <maharjah@mail.uc.edu>
2026-02-12 22:59:48 -0500

    SalesTax

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        double priceOfAPurchase = 67.67;
+        double priceOfAPurchase = 67.7;
         double salesTax = 0.05 * priceOfAPurchase;
         System.out.println("The price of the purchase is "+ priceOfAPurchase + " and the sales tax is "+ salesTax);
     }
